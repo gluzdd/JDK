591cbf07ba37a49b528a05f38987d60e4df14186
Artem <Artem@Nesterov.ru>
2024-04-13 15:33:18 +0300

    hw6

--- src/main/java/Seminar6/Main.java
+++ src/main/java/Seminar6/Main.java
@@ -1,4 +1,30 @@
 package Seminar6;
 
+import java.util.concurrent.CountDownLatch;
+
 public class Main {
+    static CountDownLatch latch = new CountDownLatch(4);
+
+    public static void main(String[] args) {
+        Paradox game = new Paradox();
+        long startTime = System.currentTimeMillis();
+
+//        game.startGame(10_000_000);
+//        game.startGame(100_000_000);
+//        game.startGame(100_000);
+//        game.startGame(1_000_000);
+
+        new Thread(() -> game.startGame(1_000_000)).start();
+        new Thread(() -> game.startGame(10_000_000)).start();
+        new Thread(() -> game.startGame(100_000_000)).start();
+        new Thread(() -> game.startGame(100_000)).start();
+
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            System.out.println("Interrupted");
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println("Time: " + (endTime - startTime));
+    }
 }
